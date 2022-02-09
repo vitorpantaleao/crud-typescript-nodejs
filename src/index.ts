6290da7ea1e6c6ada40594655cdcae969a27b1e7
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-08 23:59:42 -0300

    adiciona todas as rotas e refatora rota de status

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
+import statusRoute from './routes/status.route'
 import usersRoute from './routes/users.route'
 
 const app = express()
 
-app.use(usersRoute)
+// Config da aplicação
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+// Config de rotas
+app.use(usersRoute)
+app.use(statusRoute)
+
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000')
 })
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -19,9 +19,22 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
     res.status(StatusCodes.OK).send({ uuid })
 })
 
-usersRoute.post('/users', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     const newUSer = req.body
     res.status(StatusCodes.CREATED).send(newUSer)
+})
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    const modifiedUser = req.body
+
+    modifiedUser.uuid = uuid
+    res.status(StatusCodes.OK).send(modifiedUser)
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    res.sendStatus(StatusCodes.OK)
 })
 
 export default usersRoute
