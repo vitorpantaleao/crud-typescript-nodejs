f16cd1e005fa0bfac9c02e380979efffa93f1200
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-08 23:37:20 -0300

    adiciona pacote http status e cria primeiro rota post

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
 
 // get /users
 // get /users/:uuid
@@ -10,12 +11,17 @@ const usersRoute = Router()
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Vitor' }]
-    res.status(200).json({ users })
+    res.status(StatusCodes.OK).json({ users })
 })
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid
-    res.status(200).send({ uuid })
+    res.status(StatusCodes.OK).send({ uuid })
+})
+
+usersRoute.post('/users', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const newUSer = req.body
+    res.status(StatusCodes.CREATED).send(newUSer)
 })
 
 export default usersRoute
